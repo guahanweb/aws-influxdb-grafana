84c19f3f66ebe3ab6b86bfb044afafd6cec0d646
Garth Henson <garth@guahanweb.com>
2024-01-17 08:34:53 -0500

    fixing the local connection and provisioning for Grafana -> InfluxDB

--- scripts/grafana/settings.sh
+++ scripts/grafana/settings.sh
@@ -3,6 +3,7 @@ export INFLUXDB_USERNAME=admin
 export INFLUXDB_PASSWORD=this-is-a-longer-p@ssw0rd
 export INFLUXDB_ORG=vrlabs
 export INFLUXDB_BUCKET=telemetry
+export INFLUXDB_TOKEN=my-super-secret-auth-token
 
 export GRAFANA_USERNAME=admin
 export GRAFANA_PASSWORD=admin
